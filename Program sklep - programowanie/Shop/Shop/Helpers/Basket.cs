56c9c8e51670163b93657be37a12dfd791434fe8
KamilDudek00 <128988678+KamilDudek00@users.noreply.github.com>
2024-01-23 18:59:29 +0100

    Add files via upload

---
+++ Program sklep - programowanie/Shop/Shop/Helpers/Basket.cs
@@ -0,0 +1,38 @@
+﻿using Shop.Models;
+using System.Data;
+
+namespace Shop.Helpers
+{
+    public static class Basket
+    {
+        public static List<Product> products;
+        public static double productsCost;
+        public static int productsAmount;
+        public static void AddProduct(Product product)
+        {
+            if(products == null)
+            {
+                products = new List<Product>();
+            }
+
+            products.Add(product);
+            productsCost += product.Price * product.Amount;
+            productsAmount += product.Amount;
+        }
+        public static void RemoveProduct(int index)
+        {
+            var product = products[index];
+            productsCost -= product.Price * product.Amount;
+            productsAmount -= product.Amount;
+            products.RemoveAt(index);
+            
+        }
+
+        public static void Clear()
+        {
+            products.Clear();
+            productsCost = 0;
+            productsAmount = 0;
+        }
+    }
+}
---
+++ Program sklep - programowanie/Shop/Shop/Models/Order.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shop.Models
+{
+    public class Order
+    {
+        [Key]
+        public int Id { get; set; }
+        public virtual User User { get; set; }
+        public int UserId { get; set; }
+        public OrderStatus Status { get; set; }
+        public int Amount { get; set; }
+        public double Price { get; set; }
+        public virtual ICollection<ProductOrder> ProductOrders { get; set; }
+    }
+
+    public enum OrderStatus
+    {
+        Złożone,
+        Zrealizowane        
+    }
+}
